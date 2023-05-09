3cf7d4b4270ae4dcb0c1694e2021f8a5fca27d79
John <jjpyae@gmail.com>
2023-05-09 10:14:23 -0700

    Phase 1 complete

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,29 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name);
+  }
+}
+
+//local testing
+const puff = new Dragon("Puff", "green");
+console.log(puff); //Dragon { name: 'Puff', color: 'green' }
+console.log(puff.breathesFire()); //Puff breathes fire everywhere! BURN!!!!
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);//Dragon { name: 'Toothless', color: 'black' }
+console.log(toothless.breathesFire());//Toothless breathes fire everywhere! BURN!!!!
+
+console.log(Dragon.getDragons(puff, toothless));//[ 'Puff', 'Toothless' ]
+//
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
